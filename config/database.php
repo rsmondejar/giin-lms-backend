317d5f1342b67e97501beb7371874b84a63bd7e5
Raul Sanchez Mondejar <raulsm@outlook.com>
2023-01-12 18:28:55 +0100

    LMS-22 Add database NoSQL MongoDB docker

--- config/database.php
+++ config/database.php
@@ -93,6 +93,17 @@ return [
             // 'trust_server_certificate' => env('DB_TRUST_SERVER_CERTIFICATE', 'false'),
         ],
 
+        'mongodb' => [
+            'driver' => 'mongodb',
+            'host' => env('DB_MONGO_HOST', '127.0.0.1'),
+            'port' => env('DB_MONGO_PORT', '27017'),
+            'database' => env('DB_MONGO_DATABASE'),
+            'username' => env('DB_MONGO_USERNAME'),
+            'password' => env('DB_MONGO_PASSWORD'),
+            'options' => [
+                'database' => env('DB_MONGO_AUTHENTICATION_DATABASE', 'admin'),
+            ],
+        ],
     ],
 
     /*
